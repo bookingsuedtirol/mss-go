1b65e99bddaf169246a3ebf55081331023a54e7f
Thomas Walter <thomas.walter@hgv.it>
2022-11-08 12:27:47 +0100

    Add Result.Form

--- response/response.go
+++ response/response.go
@@ -563,6 +563,7 @@ type Result struct {
 	Locations []Location      `xml:"location"`
 	Themes    []ThemeListItem `xml:"theme"`
 	Booking   Booking         `xml:"booking"`
+	Form      Form            `xml:"form"`
 	Coupon    Coupon          `xml:"coupon"`
 }
 
